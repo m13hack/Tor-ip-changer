719f272aa114467229b13050de863c9181966d8d
m13hack <119548847+MADHUVANTHI2004@users.noreply.github.com>
2024-07-24 13:05:36 +0530

    Update installer_script.py

--- installer_script.py
+++ installer_script.py
@@ -4,16 +4,14 @@ import subprocess
 def install_script():
     """Install the Tor IP Changer script."""
     try:
-        # Set executable permissions and create directory
+        print("[+] Installing Tor IP Changer script...")
         os.chmod('Tor.py', 0o755)
         os.makedirs('/usr/share/aut', exist_ok=True)
-        
-        # Copy script and create the command file
         subprocess.run(['cp', 'Tor.py', '/usr/share/aut/Tor.py'], check=True)
+
         with open('/usr/bin/aut', 'w') as file:
             file.write('#!/bin/sh\nexec python3 /usr/share/aut/Tor.py "$@"')
         
-        # Set executable permissions
         os.chmod('/usr/bin/aut', 0o755)
         os.chmod('/usr/share/aut/Tor.py', 0o755)
 
@@ -25,6 +23,7 @@ From now, just type \x1b[6;30;42maut\x1b[0m in terminal.''')
 def uninstall_script():
     """Uninstall the Tor IP Changer script."""
     try:
+        print("[+] Uninstalling Tor IP Changer script...")
         subprocess.run(['rm', '-rf', '/usr/share/aut', '/usr/bin/aut'], check=True)
         print('[!] Auto Tor IP Changer has been removed successfully.')
     except Exception as e:
@@ -40,5 +39,5 @@ def main():
     else:
         print('[!] Invalid choice. Please press Y to install or N to uninstall.')
 
-if __name__ == "__main__":
-    main()
+if _name_ == "_main_":
+    main()
