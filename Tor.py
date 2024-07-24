a777b8c839f0c2c69cc330649f97e843523c031a
m13hack <119548847+MADHUVANTHI2004@users.noreply.github.com>
2024-07-24 13:04:24 +0530

    Update Tor.py

--- Tor.py
+++ Tor.py
@@ -1,51 +1,55 @@
 import time
 import os
 import subprocess
+import requests
 
+def check_installation():
+    """Check and install necessary dependencies."""
+    try:
+        check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
+        if 'install ok installed' not in str(check_pip3):
+            raise subprocess.CalledProcessError(1, 'pip3 not installed')
+    except subprocess.CalledProcessError:
+        print('[+] pip3 not installed')
+        subprocess.check_output('sudo apt update', shell=True)
+        subprocess.check_output('sudo apt install python3-pip -y', shell=True)
+        print('[!] pip3 installed successfully')
 
+    try:
+        import requests
+    except ImportError:
+        print('[+] python3 requests is not installed')
+        os.system('pip3 install requests')
+        os.system('pip3 install requests[socks]')
+        print('[!] python3 requests is installed')
 
+    try:
+        check_tor = subprocess.check_output('which tor', shell=True)
+        if not check_tor:
+            raise subprocess.CalledProcessError(1, 'tor not installed')
+    except subprocess.CalledProcessError:
+        print('[+] tor is not installed!')
+        subprocess.check_output('sudo apt update', shell=True)
+        subprocess.check_output('sudo apt install tor -y', shell=True)
+        print('[!] tor is installed successfully')
 
-try:
-    check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
-    if str('install ok installed') in str(check_pip3):
-        pass
-except subprocess.CalledProcessError:
-    print('[+] pip3 not installed')
-    subprocess.check_output('sudo apt update',shell=True)
-    subprocess.check_output('sudo apt install python3-pip -y', shell=True)
-    print('[!] pip3 installed succesfully')
+    os.system("clear")
 
-
-
-try:
-
-    import requests
-except Exception:
-    print('[+] python3 requests is not installed')
-    os.system('pip3 install requests')
-    os.system('pip3 install requests[socks]')
-    print('[!] python3 requests is installed ')
-try:
-
-    check_tor = subprocess.check_output('which tor', shell=True)
-except subprocess.CalledProcessError:
-
-    print('[+] tor is not installed !')
-    subprocess.check_output('sudo apt update',shell=True)
-    subprocess.check_output('sudo apt install tor -y',shell=True)
-    print('[!] tor is installed succesfully ')
-
-os.system("clear")
 def ma_ip():
-    url='https://www.myexternalip.com/raw'
-    get_ip= requests.get(url,proxies=dict(http='socks5://127.0.0.1:9050',https='socks5://127.0.0.1:9050'))
+    """Get current external IP using Tor."""
+    url = 'https://api.ipify.org?format=text'
+    get_ip = requests.get(url, proxies=dict(http='socks5://127.0.0.1:9050', https='socks5://127.0.0.1:9050'))
     return get_ip.text
 
 def change():
+    """Change Tor IP."""
     os.system("service tor reload")
-    print ('[+] Your IP has been Changed to : '+str(ma_ip()))
+    print('[+] Your IP has been Changed to: ' + str(ma_ip()))
 
-print('''\033[1;32;40m \n
+def main():
+    check_installation()
+
+    print('''\033[1;32;40m \n
                 _          _______
      /\        | |        |__   __|
     /  \  _   _| |_ ___      | | ___  _ __
@@ -55,30 +59,29 @@ print('''\033[1;32;40m \n
                 V 2.1
 from mrFD
 ''')
-print("\033[1;40;31m http://facebook.com/ninja.hackerz.kurdish/\n")
+    print("\033[1;40;31m http://facebook.com/ninja.hackerz.kurdish/\n")
 
-os.system("service tor start")
+    os.system("service tor start")
 
+    time.sleep(3)
+    print("\033[1;32;40m change your SOCKS to 127.0.0.1:9050 \n")
+    os.system("service tor start")
 
+    x = input("[+] Time to change IP in Sec [type=60] >> ")
+    lin = input("[+] How many times do you want to change your IP [type=1000] for infinite IP change type [0] >> ")
 
+    if int(lin) == 0:
+        while True:
+            try:
+                time.sleep(int(x))
+                change()
+            except KeyboardInterrupt:
+                print('\nAuto Tor is closed')
+                quit()
+    else:
+        for i in range(int(lin)):
+            time.sleep(int(x))
+            change()
 
-time.sleep(3)
-print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
-os.system("service tor start")
-x = input("[+] time to change Ip in Sec [type=60] >> ")
-lin = input("[+] how many time do you want to change your ip [type=1000]for infinte ip change type [0] >>")
-if int(lin) ==int(0):
-
-	while True:
-		try:
-			time.sleep(int(x))
-			change()
-		except KeyboardInterrupt:
-
-		 	print('\nauto tor is closed ')
-		 	quit()
-
-else:
-	for i in range(int(lin)):
-		    time.sleep(int(x))
-		    change()
+if __name__ == "__main__":
+    main()
