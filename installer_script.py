e92c9e3706f3c9e0b2752626085281b5066e4d52
m13hack <119548847+MADHUVANTHI2004@users.noreply.github.com>
2024-07-24 17:50:04 +0530

    Update installer_script.py

--- installer_script.py
+++ installer_script.py
@@ -13,10 +13,9 @@ def install_script():
             file.write('#!/bin/sh\nexec python3 /usr/share/aut/Tor.py "$@"')
         
         os.chmod('/usr/bin/aut', 0o755)
-        os.chmod('/usr/share/aut/Tor.py', 0o755)
 
         print('''\n\nCongratulations! Auto Tor IP Changer is installed successfully.
-From now, just type \x1b[6;30;42maut\x1b[0m in terminal.''')
+From now, just type \033[1;32maut\033[0m in terminal.''')
     except Exception as e:
         print(f'[!] Installation failed: {e}')
 
@@ -30,14 +29,14 @@ def uninstall_script():
         print(f'[!] Uninstallation failed: {e}')
 
 def main():
-    choice = input('[+] To install press (Y) to uninstall press (N) >> ').strip().lower()
+    choice = input('[+] Ready to roll? Type \033[1;32mY\033[0m to install or \033[1;31mN\033[0m to uninstall >> ').strip().lower()
     
     if choice == 'y':
         install_script()
     elif choice == 'n':
         uninstall_script()
     else:
-        print('[!] Invalid choice. Please press Y to install or N to uninstall.')
+        print('[!] Invalid choice. Please type \033[1;32mY\033[0m to install or \033[1;31mN\033[0m to uninstall.')
 
 if __name__ == "__main__":
     main()
